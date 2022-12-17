34dfa7e54d51d83b13fa7dd70986636426bf58fe
TiagoM13 <thyagomotha2000@gmail.com>
2022-12-17 12:07:43 -0300

    feat: add form to create ads

--- src/components/CreateAdBanner.tsx
+++ src/components/CreateAdBanner.tsx
@@ -1,7 +1,8 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
-import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
 
-export const CreateAdBanner: React.FC = () => {
+
+export const CreateAdBanner = () => {
   return (
     <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
       <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
@@ -10,10 +11,10 @@ export const CreateAdBanner: React.FC = () => {
           <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
         </div>
 
-        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 transition-all text-white rounded flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 transition-all text-white rounded flex items-center gap-3">
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   );
